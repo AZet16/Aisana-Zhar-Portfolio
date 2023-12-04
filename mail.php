21562eec23ecf691ec557ba769dfa618bce934bc
AisanaZh177 <101815204+AisanaZh177@users.noreply.github.com>
2023-12-04 00:03:47 +0000

    using message php

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+    $name= $_POST['From'];
+    $email= $_POST['Email'];
+    $profile= $_POST['Profile'];
+    $subject= $_POST['Subject'];
+    $message= $_POST['Message'];
+
+    $mailheader="From: ".$name."<".$email.">\r\n";
+    $finalmessage=.$message."\n".$profile.;
+
+    $recipient="aisana16zh@gmail.com";
+
+    mail($recipient,$subject,$finalmessage,$mailheader)
+    or die("Error");
+
+    echo "message sent";
+
+?>
